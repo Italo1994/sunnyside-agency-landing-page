ec566f969f349ffb092d3ae5d08bd815490e1ed0
Italo1994 <italohortiz2015@gmail.com>
2021-08-08 23:19:10 -0300

    Add the section categories, testimonial and the gallery of images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CategoriesComponent,
+    TestimonialsComponent,
+    GalleryComponent,
   ],
   imports: [
     BrowserModule,
